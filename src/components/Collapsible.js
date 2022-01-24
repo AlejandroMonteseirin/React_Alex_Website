d52881a2bc94a72d478e029f505f7ff560b9925f
AlejandroMonteseirin <alexmonteseirin@gmail.com>
2022-01-24 21:29:55 +0100

    CI/CD

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,13 +1,44 @@
 import * as React from 'react';
 import logo from '../logo.svg';
 import ListElement from './ListElement';
+import ssrl from '../images/ssrl.png';
+import pokemon from '../images/pokemon.PNG';
+import github from '../images/GitHub.png';
 
-var datosGames= [{title:"title",description:"info",link:"www.google.es",linkText:"Enlace"},{title:"title2",info:"info2"}];
-console.log(datosGames);
+var datosGames= [
+    {title:"SpaceShip Roguelike",fecha:"2022" ,description:"Juego profesional para Android de naves, estilo rogueLike en el que juegas partidas cortas y freneticas. Desplegado en la play store, conectado a la api de google play juegos y monetizado mediante Admob (anuncios)",link:"https://play.google.com/store/apps/details?id=org.godotengine.spaceRogueLikeASB",linkText:"Enlace a la play Store",image:ssrl},
+    {title:"Epic Chili Android",fecha:"2017" ,description:"Juego RPG completo de historia Sevillana para Android, basado en RPG Maker",link:"https://drive.google.com/file/d/1iAv-6_dE43gAGHwUR8VGJkFmSaegtCW5/view",linkText:"Enlace a la descarga"},
+    {title:"Pokemon Sin nombre oficial aun :) ",fecha:"2016",description:"Demo Juego de PC no profesional de Pokemon totalmente customizado, basado en RPG maker muy scripteado",link:"https://mega.nz/file/ZNJkEBrA#iz0PVXq2U6u6IzNaSpVSIeMi87W-oBqvdGVJRoiqU-I",linkText:"Enlace a la descarga",image:pokemon},
+    {title:"Sueco el sueco",fecha:"2012",description:"Prototipo RPG basico",link:"https://mega.nz/file/UZRmnTxJ#xqYLlOcM-kG4Q0KPpOyz6A5iIFOeu9qDIEiJ6MSQZv0",linkText:"Enlace a la descarga"}
+
+
+
+
+];
 
 const elementsArrayGames= datosGames.map((item,pos)=>{
     return (
-      <ListElement title={item.title} description={item.description} link={item.link} linkText={item.linkText}></ListElement>
+      <ListElement key={item.title} title={item.title} description={item.description} link={item.link} linkText={item.linkText} image={item.image} fecha={item.fecha}></ListElement>
+    )
+})
+
+var datosProyectos= [
+    {title:"React Website",fecha:"2022",description:"Literalmente esta pagina, hecha con objetivos formativos siguiendo el curso react-js-a-complete-guide-for-frontend-web-development de Udemy, react + materialicecss,desplegada en firebase",link:"https://github.com/AlejandroMonteseirin/React_Alex_Website",linkText:"Enlace a github",image:github},
+    {title:"Angular_Testing_Karma_Jasmine",fecha:"2021",description:"Prototipo de repositorio de una aplicación Angular11+ con Test Automatizados utilizando Karma Jasmine",link:"https://github.com/AlejandroMonteseirin/Angular_Testing_Karma_Jasmine",linkText:"Enlace a github",image:github},
+    {title:"Plantilla Angular",fecha:"2020-2022" ,description:"Proyecto plantilla de Angular 10+ con los componentes NgPrime, ngx-translate y otros preparados. Utilizado para agilizar la creación de un nuevo proyecto Angular",link:"https://github.com/AlejandroMonteseirin/ProyectoPlantillaAngular10NgPrime",linkText:"Enlace a github",image:github},
+    {title:"Backend Node.js",fecha:"2020",description:"Prototipo de Backend basado en Node.js, siguiendo el curso Curso de node.js en Platzi.com",link:"https://github.com/AlejandroMonteseirin/Backend-Node.js",linkText:"Enlace a github",image:github},
+    {title:"Django Pagina Personal",fecha:"2020",description:"Pagina web en basada en Django, conectada a una base de datos Postgresql y desplegada mediante Heroku",link:"https://github.com/AlejandroMonteseirin/Django_Personal_Web",linkText:"Enlace a github",image:github},
+    {title:".NetCore Web Api",fecha:"2020",description:"Proyecto de carácter formativo, en el cual se desarrolla una Api rest basada en ASP NET CORE, implementando JWT(JSON Web Token), las migraciones utilizando el entity framework y las operaciones bás…",link:"https://github.com/AlejandroMonteseirin/.net_core_Web_api",linkText:"Enlace a github",image:github},
+    {title:"Test Pokemon",fecha:"2020",description:"Pagina parodia de un test de personalidad pokemon, Angular + Firebase",link:"https://github.com/AlejandroMonteseirin/TestPokemon.com",linkText:"Enlace a github",image:github},
+
+
+
+
+];
+
+const elementsArrayProyectos= datosProyectos.map((item,pos)=>{
+    return (
+      <ListElement key={item.title} title={item.title} description={item.description} link={item.link} linkText={item.linkText} image={item.image} fecha={item.fecha} isGithub="true"></ListElement>
     )
 })
 console.log(elementsArrayGames);
@@ -19,7 +50,7 @@ export default function Collapsible() {
             <ul className="collapsible" style={{  borderColor:"#282C34"}}>
                 <li className="active">
                     <div className="collapsible-header" style={{ backgroundColor:"#224E54",borderColor:"#282C34"}}><i className="material-icons">book</i>Proyects</div>
-                    <div className="collapsible-body">  <ListElement title="TEST2"></ListElement></div>
+                    <div className="collapsible-body"> {elementsArrayProyectos}</div>
                 </li>
                 <li >
                     <div className="collapsible-header" style={{ backgroundColor:"#224E54",borderColor:"#282C34"}}><i className="material-icons">videogame_asset</i>Games</div>
@@ -27,8 +58,9 @@ export default function Collapsible() {
                 </li>
                 <li>
                     <div className="collapsible-header" style={{ backgroundColor:"#224E54",borderColor:"#282C34"}}><i className="material-icons">grade</i>Others</div>
-                    <div className="collapsible-body">  <ListElement title="TEST2"></ListElement></div>
+                    <div className="collapsible-body"> Aqui no hay nada aún... </div>
                 </li>
+                
             </ul>
         </div>)
 };
--- src/components/ListElement.js
+++ src/components/ListElement.js
@@ -1,16 +1,38 @@
 import * as React from 'react';
 import classes from '../css/PageCss.module.css'
 
-const ListElement = (properties)=>{
+const ListElement = (properties) => {
 
+    var imagen=<div className="card-title" ><div style={{padding:"1em",fontSize:"larguer",fontWeight:"bolder"}}>{properties.title} </div></div>;
+    if (properties.image!=null && properties.isGithub){
+        imagen= <div className="card-image"><img style={{width:"30%",left:"60%",paddingTop:"1em"}} src={properties.image}/><span className="card-title" style={{position:"inherit",paddingTop:"1em",fontSize:"larguer",fontWeight:"bolder"}}>{properties.title}</span></div> 
+
+    }else if(properties.image!=null){
+        imagen= <div className="card-image"><img src={properties.image}/><span className="card-title" style={{position:"inherit",paddingTop:"1em",fontSize:"larguer",fontWeight:"bolder"}}>{properties.title}</span></div> 
+
+    }
     return (
-    <div className={classes.cardcss}>
-        <h4>{properties.title}</h4>
-        <p>{properties.description}</p>
-        <a style={{color:"blue"}} href ={properties.link}> {properties.linkText}</a>
-    </div>
-    )
 
+        <div  className="card" style={{ backgroundColor: "#14969A", marginBottom:"3em" }}>
+            {imagen}
+            <div className="card-content">
+            <div style={{color:"red", fontWeight:"bold",fontFamily:"cursive",fontSize:"large"}}>{properties.fecha}</div>
+
+                <p>{properties.description}</p>
+            </div>
+            <div className="card-action">
+                <a href={properties.link}>{properties.linkText}</a>
+            </div>
+        </div>
+
+
+    )
+    /*
+    <div className={classes.cardcss}>
+            <h4>{properties.title}</h4>
+            <p>{properties.description}</p>
+            <a style={{color:"blue"}} href ={properties.link}> {properties.linkText}</a>
+        </div>*/
 
 }
 export default ListElement;
